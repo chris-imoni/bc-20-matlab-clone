79297144fecf6289f8c8093b5e93b1baaa05a639
Chris <talk2chris2030@yahoo.com>
2017-03-23 11:52:59 +0100

    file updated to include a function for matrix and number addition

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ str,
 result;
 
 input = readlineSync.question(
-    'Enter 1 to create array\nEnter 2 to create a Matrix \nEnter 3 to create a vector of zeros\n' 
+    'Enter 1 to create array\nEnter 2 to create a Matrix \nEnter 3 to create a vector of zeros\nEnter 4 for matrix and number addition\n' 
     );
 //Converting what the user enter to integer
 num = parseInt(input);
@@ -64,6 +64,26 @@ if(num == 1){
     console.log("\n");
     console.log(result);
 
+}else if(num == 4){
+    str = readlineSync.question('Enter numbers separted by comma for each row, separate each row with a semicolon\n');
+    while(str == "" || str == null){
+        console.log("Invalid entry. Try again");
+         str = readlineSync.question('Enter numbers separted by comma for each row, separate each row with a semicolon\n');
+    }
+    var arr = str.split(";");
+    str = readlineSync.question('Enter a value for the addition\n');
+    while(isNaN(str)){
+        console.log("Ivalid entry! Enter a number");
+        str = readlineSync.question('Enter a value for the addition\n');
+    }
+    var b = parseInt(str);
+    result = mat_add(arr, b);
+    if(!result){
+        console.log("Oops! Your array should contain numbers only. Try again");
+    }else{
+        console.log("\n");
+        console.log(result);
+    }
 }else{
     console.log("You entered an invalid command. Try again");
 }
@@ -136,5 +156,30 @@ function vector_zeros(rows, cols){
 	 output += "\n";
 	}
 
+	return output;
+}
+
+//Function for adding a number to a matrix
+function mat_add(arr, b){
+	output="", 
+	a = [], 
+	rows = [],
+	row1 = [];
+	for(var row=0; row < arr.length; row++){
+		a[row] = [];
+		rows[row] = arr[row];
+		row1 = rows[row].split(",");
+        if(!ValidateArr(row1)){
+            return false;
+        }
+		for(var col=0; col < row1.length; col++){
+			a[row][col] = parseInt(row1[col]) + b;
+			output += a[row][col] +"\t";
+		}
+
+		output += '\n';
+	}
+	
+	
 	return output;
 }
