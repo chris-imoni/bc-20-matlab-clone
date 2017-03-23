8e111270a960fbd16997358d8eada7dd951c6574
Chris <talk2chris2030@yahoo.com>
2017-03-24 00:40:02 +0100

    update file to include a transpose function

--- index.js
+++ index.js
@@ -1,108 +1,149 @@
 var readlineSync = require('readline-sync');
  
 // Wait for user's response.
-var input = "",
-num,
-str,
-result;
+var num,
+	str="",
+	result;
 
-input = readlineSync.question(
-    'Enter 1 to create array\nEnter 2 to create a Matrix \nEnter 3 to create a vector of zeros\n'+
+start_lab();
+
+function start_lab(){
+	str = readlineSync.question(
+    'Enter 1 to create array\n'+
+	'Enter 2 to create a Matrix \n'+
+	'Enter 3 to create a vector of zeros\n'+
     'Enter 4 for matrix and number addition\n'+
-    'Enter 5 for transposing a matrix\n' 
+    'Enter 5 for transposing a matrix\n'+
+	'Enter zero (0) to terminate\n'
     );
-//Converting what the user enter to integer
-num = parseInt(input);
+	//Converting what the user enter to integer
+	num = parseInt(str);
 
+	if(num == 0){
+		console.log();
+	}
 
-if(num == 1){
-     str = readlineSync.question('Enter list of numbers separted by comma\n');
-    while(str == "" || str == null){
-        console.log("Invalid entry. Try again");
-         str = readlineSync.question('Enter list of numbers separted by comma\n');
-    }
+	else if(num == 1){
+		str = readlineSync.question('\nEnter list of numbers separted by comma\n');
+		while(str == "" || str == null){
+			console.log("Invalid entry. Try again");
+			str = readlineSync.question('Enter list of numbers separted by comma\n');
+		}
 
-    var arr = str.split(",");
-    if(ValidateArr(arr)){
-        result = createArr(arr);
-        console.log("["+result.trim(" ") +"]");
-    }else{
-        console.log("Oops! Your array should contain numbers only. Try again");
-    }
+		var arr = str.split(",");
+		if(ValidateArr(arr)){
+			result = createArr(arr);
+			console.log("\n["+result.trim(" ") +"]");
+			//giving the user more options
+			lab_option();
+		}else{
+			console.log("Oops! Your array should contain only numbers. Try again");
+			lab_option();
+		}
 
+	}
+	
+	else if(num == 2){
+		str = readlineSync.question('Enter numbers separted by comma for each row, separate each row with a semicolon\n');
+		while(str == "" || str == null){
+			console.log("Invalid entry. Try again");
+			str = readlineSync.question('Enter numbers separted by comma for each row, separate each row with a semicolon\n');
+		}
+		var arr = str.split(";");
+		result = createMatrix(arr);
+		if(!result){
+			console.log("Oops! Your array should contain only numbers. Try again\n");
+			lab_option();
+		}else{
+			console.log("\n");
+			console.log(result);
+			lab_option();
+		}
 
-}else if(num == 2){
-     str = readlineSync.question('Enter numbers separted by comma for each row, separate each row with a semicolon\n');
-    while(str == "" || str == null){
-        console.log("Invalid entry. Try again");
-         str = readlineSync.question('Enter numbers separted by comma for each row, separate each row with a semicolon\n');
-    }
-    var arr = str.split(";");
-    result = createMatrix(arr);
-    if(!result){
-        console.log("Oops! Your array should contain numbers only. Try again");
-    }else{
-        console.log(result);
-    }
+	}
+	
+	else if(num == 3){
+		str = readlineSync.question('Enter value for row\n');
+		while(isNaN(str) || str == 0){
+			console.log("Invalid entry! only number other than zero is allowed");
+			str = readlineSync.question('Enter value for row\n');
+		}
 
+		var r = parseInt(str);
 
-}else if(num == 3){
-    str = readlineSync.question('Enter value for row\n');
-    while(isNaN(str) || str == 0){
-        console.log("Invalid entry! only number other than zero is allowed");
-        str = readlineSync.question('Enter value for row\n');
-    }
+		str = readlineSync.question('Enter value for column\n');
+		while(isNaN(str) || str == 0){
+			console.log("Invalid entry! only number other than zero is allowed");
+			str = readlineSync.question('Enter value for column\n');
+		}
 
-    var r = parseInt(str);
+		var c = parseInt(str);
+		result = vector_zeros(r,c);
+		console.log("\n");
+		console.log(result);
+		lab_option();
+	}
+	
+	else if(num == 4){
+		str = readlineSync.question('Enter numbers separted by comma for each row, separate each row with a semicolon\n');
+		while(str == "" || str == null){
+			console.log("Invalid entry. Try again");
+			str = readlineSync.question('Enter numbers separted by comma for each row, separate each row with a semicolon\n');
+		}
+		var arr = str.split(";");
+		str = readlineSync.question('Enter a value for the addition\n');
+		while(isNaN(str)){
+			console.log("Ivalid entry! Enter a number");
+			str = readlineSync.question('Enter a value for the addition\n');
+		}
+		var b = parseInt(str);
+		result = mat_add(arr, b);
+		if(!result){
+			console.log("Oops! Your array should contain numbers only. Try again");
+			lab_option();
+		}else{
+			console.log("\n");
+			console.log(result);
+			lab_option();
+		}
+	}
+	
+	else if(num == 5){
+		str = readlineSync.question('Enter numbers separted by comma for each row, separate each row with a semicolon\n');
+		while(str == "" || str == null){
+			console.log("Invalid entry. Try again");
+			str = readlineSync.question('Enter numbers separted by comma for each row, separate each row with a semicolon\n');
+		}
+		result = matrix_trans(str);
+		if(!result){
+			console.log("Oops! Your array should contain numbers only. Try again");
+			lab_option();
+		}else{
+			console.log("\n");
+			console.log(result);
+			lab_option();
+		}
+	}
+	
+	else{
+		console.log("You entered an invalid command. Try again");
+		lab_option();
+	}
 
-    str = readlineSync.question('Enter value for column\n');
-    while(isNaN(str) || str == 0){
-        console.log("Invalid entry! only number other than zero is allowed");
-        str = readlineSync.question('Enter value for column\n');
-    }
-
-    var c = parseInt(str);
-    result = vector_zeros(r,c);
-    console.log("\n");
-    console.log(result);
-
-}else if(num == 4){
-    str = readlineSync.question('Enter numbers separted by comma for each row, separate each row with a semicolon\n');
-    while(str == "" || str == null){
-        console.log("Invalid entry. Try again");
-         str = readlineSync.question('Enter numbers separted by comma for each row, separate each row with a semicolon\n');
-    }
-    var arr = str.split(";");
-    str = readlineSync.question('Enter a value for the addition\n');
-    while(isNaN(str)){
-        console.log("Ivalid entry! Enter a number");
-        str = readlineSync.question('Enter a value for the addition\n');
-    }
-    var b = parseInt(str);
-    result = mat_add(arr, b);
-    if(!result){
-        console.log("Oops! Your array should contain numbers only. Try again");
-    }else{
-        console.log("\n");
-        console.log(result);
-    }
-}else if(num == 5){
-    str = readlineSync.question('Enter numbers separted by comma for each row, separate each row with a semicolon\n');
-    while(str == "" || str == null){
-        console.log("Invalid entry. Try again");
-         str = readlineSync.question('Enter numbers separted by comma for each row, separate each row with a semicolon\n');
-    }
-    result = matrix_trans(str);
-    if(!result){
-        console.log("Oops! Your array should contain numbers only. Try again");
-    }else{
-        console.log("\n");
-        console.log(result);
-    }
-}else{
-    console.log("You entered an invalid command. Try again");
 }
 
+//Giving the user the option to exit or start afresh
+function lab_option(){
+	console.log("\n");
+	str = readlineSync.question("Enter zero (0) to terminate or 9 to restart\n");
+	num = parseInt(str);
+	if(num == 9){
+		start_lab();
+	}else{
+		console.log();
+	}
+
+}
 
 //Making sure the user inputs only numbers
 function ValidateArr(arr){
@@ -128,10 +169,10 @@ function createArr(arr){
 
 //Function for creating a Matrix
 function createMatrix(arr){
-	output="", 
-	a = [], 
-	rows = [],
-	row1 = [];
+	var output="", 
+		a = [], 
+		rows = [],
+		row1 = [];
 	for(var row=0; row < arr.length; row++){
 		a[row] = [];
 		rows[row] = arr[row];
@@ -154,21 +195,21 @@ function createMatrix(arr){
 //Function for creating vector of zeros
 function vector_zeros(rows, cols){
 	var iRow = rows,
-	jCol = cols,
-	output = "",
-	f = [];
+		jCol = cols,
+		output = "",
+		f = [];
 	for (i = 0; i < iRow; i++) {
-	 f[i] = [];
-	 for (j = 0; j < jCol; j++) {
-	  f[i][j] = 0;
-	 }
+	 	f[i] = [];
+		for (j = 0; j < jCol; j++) {
+			f[i][j] = 0;
+		}
 	}
 
 	for (i = 0; i < iRow; i++) {
-	 for (j = 0; j < jCol; j++) {
-	  output += f[i][j] + "   ";
-	 }
-	 output += "\n";
+		for (j = 0; j < jCol; j++) {
+			output += f[i][j] + "   ";
+		}
+	 	output += "\n";
 	}
 
 	return output;
@@ -176,10 +217,10 @@ function vector_zeros(rows, cols){
 
 //Function for adding a number to a matrix
 function mat_add(arr, b){
-	output="", 
-	a = [], 
-	rows = [],
-	row1 = [];
+	var output="", 
+		a = [], 
+		rows = [],
+		row1 = [];
 	for(var row=0; row < arr.length; row++){
 		a[row] = [];
 		rows[row] = arr[row];
@@ -200,19 +241,17 @@ function mat_add(arr, b){
 }
 
 //Function for transposing a matrix
-matrix_trans("1,2,3;4,5,6");
-
-function matrix_trans(a){
-	var arr = a.split(";"),
-	output="", 
-	a = [], 
-	rows = [],
-	i,
-	j=0;
-	transpose = [],
-	row1 = [];
+function matrix_trans(ar){
+	var arr = ar.split(";"),
+		output="", 
+		a = [], 
+		rows = [],
+		i,
+		j=0;
+		transpose = [],
+		row1 = [];
 	
-	output += 'The matrix\n';
+	output += 'The Matrix\n';
 	for(var row=0; row < arr.length; row++){
 		a[row] = [];
 		rows[row] = arr[row];
@@ -223,10 +262,11 @@ function matrix_trans(a){
 		}
 		output += '\n';
 	}
+
 	output += '\n';
-	
+	output += "The Transpose\n";
 	i = arr.length;
-	//initializing
+	
 	for(row = 0; row < i; row++){
 		a[row] = [];
 		rows[row] = arr[row];
@@ -236,28 +276,28 @@ function matrix_trans(a){
 			transpose[col] = [];
 		}
 
-	for (var row = 0; row < arr.length; row++) {
-		a[row] = [];
-		rows[row]= arr[row];
-		row1 = rows[row].split(",");
-		for (var col = 0; col < row1.length; col++) {
-			a[row][col] = row1[col];
+		for (var row = 0; row < arr.length; row++) {
+			a[row] = [];
+			rows[row]= arr[row];
+			row1 = rows[row].split(",");
+			for (var col = 0; col < row1.length; col++) {
+				a[row][col] = row1[col];
+			}
 		}
-  }
   
-  for (var row = 0; row < i; row++) {
-    for (var col = 0; col < j; col++) {
-		transpose[col][row]= a[row][col]
-    }
-  }
+		for (var row = 0; row < i; row++) {
+			for (var col = 0; col < j; col++) {
+				transpose[col][row]= a[row][col]
+			}
+		}
   
-   for (var row = 0; row < i; row++) {
-    for (var col = 0; col < j; col++) {
-		output += transpose[row][col] + "\t";
-    }
-	output += "\n";
-  }
-}	
+		for (var row = 0; row < i; row++) {
+			for (var col = 0; col < j; col++) {
+				output += transpose[row][col] + "\t";
+			}
+			output += "\n";
+		}
+	}	
 	
 	return output;
 }
